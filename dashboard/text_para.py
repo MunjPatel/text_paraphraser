031fa0c43c7c8f6861097853b9388c3948aba8a0
MunjPatel <patelmunj2011@gmail.com>
2023-08-12 19:26:24 +0530

    changed input labels

--- dashboard/text_para.py
+++ dashboard/text_para.py
@@ -19,11 +19,11 @@ mode_name = [str(mode_name) for mode_name in mode.keys()]
 with st.form("paraphrase_form"):
     default_text = """Paraphrasing text using AI involves leveraging advanced natural language processing (NLP) techniques to rephrase sentences or passages while retaining their original meaning. AI-powered paraphrasing systems utilize large language models trained on diverse textual data to generate alternative phrasings that maintain coherence and context. These systems analyze the input text's structure, semantics, and relationships between words to produce high-quality paraphrased output. This technology finds applications in content creation, language translation, and improving the readability of complex text. By harnessing AI for paraphrasing, users can efficiently transform and enhance their written content, achieving greater clarity and engagement while preserving the intended message."""
     with st.sidebar:
-        input_text = st.text_area("Enter your text here: ", height = 100, value = default_text)
+        input_text = st.text_area("Input Text", height = 100, value = default_text)
         if not input_text:
             st.error('Please enter some text')
-        selected_lang = st.selectbox("Select paraphrase language: ", supported_langs)
-        selected_mode = st.selectbox("Select mode", mode_name)
+        selected_lang = st.selectbox("Language Selection", supported_langs)
+        selected_mode = st.selectbox("Paraphrasing Mode", mode_name)
         submit_button = st.form_submit_button("Submit")
 
 if submit_button and len(input_text) != 0:
